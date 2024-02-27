c003782db956c9b7059debbdbcaac95b9d48ff63
 Patrick <patrick@MacBook-Pro.local>
2024-02-27 15:05:08 +0200

    update

--- src/js/views/Biaographi/index.js
+++ src/js/views/Biaographi/index.js
@@ -12,26 +12,35 @@ class Biography {
                                          <h1 class="biography__title" animated>Work Experience</h1>
                                      </div>
                                      <div class="biography__wrapper">
+                                      <div class="biography__item" animated>
+                                        <p class="biography__year">2020 – 2024 <strong class="biography__organisation">Govitall / Qollabe</strong></p>
+                                        <p class="biography__position">Front end developer</p>
+                                          <ul class="biography__list">
+                                            <li class="biography__item_responsibilities">Developed and supported more than 100 company websites;</li>
+                                            <li class="biography__item_responsibilities">Speed up websites, SEO optimization and improving Google Page Speed metrics, making our websites more attractive to Google and user-friendly;</li>
+                                            <li class="biography__item_responsibilities">Increased the Performance metric of more than 20 top sites by improving the LСP CLS and FСP parameters;</li>
+                                            <li class="biography__item_responsibilities">Moved order forms and calculators from JS to React.js;</li>
+                                          </ul>
+                                      </div>
+                                      <div class="biography__item" animated>
+                                        <p class="biography__year">2018 – 2020 <strong class="biography__organisation">Digital Pirates</strong></p>
+                                        <p class="biography__position">Front end developer</p>
+                                          <p class="biography__list">
+                                          Website development for customers.
+                                          </p>
+                                      </div>
                                          <div class="biography__item" animated>
                                             <p class="biography__year">2013 – 2015 <strong class="biography__organisation">Kyivoblenergo</strong></p>
                                             <p class="biography__position">Engineer 1 category.</p>
                                               <ul class="biography__list">
-                                                <li class="biography__item_responsibilities">development of tender documentation;</li>
-                                                <li class="biography__item_responsibilities">conducting tenders (for purchasing goods);</li>
-                                                <li class="biography__item_responsibilities">development of a supplier selection strategy, supplier search and selection;</li>
-                                                <li class="biography__item_responsibilities">contractual biography: agreement and conclusion of contracts, evaluation of the supplier's biography in accordance with the concluded contracts;</li>
-                                                <li class="biography__item_responsibilities">analysis of the needs of the company, price planning;</li>
+                                                <li class="biography__item_responsibilities">Development of tender documentation and supplier selection strategy;</li>
+                                                <li class="biography__item_responsibilities">Conducting tenders;</li>
+                                                <li class="biography__item_responsibilities">Supplier search and selection;</li>
+                                                <li class="biography__item_responsibilities">Monitoring compliance with delivery deadlines and product quality;</li>
+                                                <li class="biography__item_responsibilities">Analysis of the needs of the company, price planning.</li>
                                               </ul>
                                          </div>
-                                         <div class="biography__item" animated>
-                                            <p class="biography__year">2010 – 2012   <strong class="biography__organisation"> Universal Bank</strong></p>
-                                            <p class="biography__position">IT specialist</p>
-                                            <ul class="biography__list">
-                                                <li class="biography__item_responsibilities">technical support of users;</li>
-                                                <li class="biography__item_responsibilities">server infrastructure support;</li>
-                                                <li class="biography__item_responsibilities">computer maintenance;</li>
-                                            </ul>
-                                        </div>
+                                        
                                     </div>
                                   </div> 
                                   
